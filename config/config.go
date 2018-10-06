b2bc22877d1df6516b4bc91ec8af8ef2f299809c
Krunoslav Husak <kruno@binel.hr>
2018-10-07 00:12:48 +0200

    Added logging helper.

--- config/config.go
+++ config/config.go
@@ -9,12 +9,18 @@ import (
 // Configuration struct have all fields from configuration JSON file
 type Configuration struct {
 	Server Server `json:"server"`
+	Log    Log    `json:"log"`
 }
 
 // Server defines http server configuration (address and port)
 type Server struct {
 	IPAddress string `json:"ip_address"`
 	Port      string `json:"port"`
+}
+
+// Log defines logging configuration (log filename)
+type Log struct {
+	Filename string `json:"filename"`
 }
 
 // Load loads configuration from path
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
+
 	"github.com/h00s/go-web-app/config"
+	"github.com/h00s/go-web-app/logger"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -9,12 +12,19 @@ import (
 func main() {
 	c, err := config.Load("config/configuration.json")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
+	l, err := logger.New(c.Log.Filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
 
 	r := echo.New()
 	r.Use(middleware.Logger())
 	r.Use(middleware.Recover())
 
+	l.Info("Starting server...")
 	r.Logger.Fatal(r.Start(c.Server.IPAddress + ":" + c.Server.Port))
 }
